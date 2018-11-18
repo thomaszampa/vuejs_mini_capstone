347a350a61092c61ba4a388d3b1596758ca59325
thomaszampa <thomas.zampa@gmail.com>
2018-11-18 11:39:17 -0600

    Inital Commit -- Working connections between frontend and blackend

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <div class="pos-f-t">
+        <div class="collapse" id="navbarToggleExternalContent">
+          <div class="bg-dark p-4">
+            <h5 class="text-white h4">Collapsed content</h5>
+            <span class="text-muted">Toggleable via the navbar brand.</span>
+          </div>
+        </div>
+        <nav class="navbar navbar-dark bg-dark">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </nav>  
+      </div>
     <router-view/>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,49 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class = "home">
+    <div class="header_image">
+      <img src="http://musicwhore.org/wp-content/uploads/2014/08/20131111_142254.jpg" class="img-fluid" alt="Responsive image">
+    </div>
+
+    <div class="container">
+      <div class="row mb-4 mt-4">
+        <div v-for="product in products" class="col-4 mt-4 mb-4 ">
+          <div class="card" style="width: 18rem;">
+            <img class="card-img-top" v-bind:src="product.images[0]" alt="Card image cap">
+            <div class="card-body">
+              <h3 class="card-title">{{ product.name }}</h3>
+              <p class="card-price">$ {{ product.price }}</p>
+              <p class="card-text">{{ product.description }}</p>
+              <a href="#" class="btn btn-primary">Add To Cart</a>
+            </div>
+          </div>
+        </div>
+      </div>  
+    </div> 
   </div>
 </template>
 
+<style>
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+var axios = require("axios");
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  data : function() {
+    return {
+      message: "Welcome to Vue.js!",
+      products: []
+    };
+  },
+  created: function() {
+    axios.get("http://localhost:3000/api/products").then(
+      function(response) {
+        console.log(response.data);
+        this.products = response.data;
+      }.bind(this)
+    );
+  },
+  methods: {},
+  computed: {}
+};
+</script> 
